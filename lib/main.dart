a443055581bbd1a72b553fc1ea415b44eddd07eb
mahmoud-eslami <eslamimahmoud98@gmail.com>
2021-03-01 21:39:57 +0330

    colors added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_wallpaper/views/home_screen/home_screen.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,13 +9,13 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/recources/app_colors/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const lightThemePrimaryColor = Color(0xffFF8552);
+  static const lightThemeBackgroundColor = Color(0xffE6E6E6);
+  static const lightThemeTextColor = Color(0xff39393A);
+  static const darkThemePrimaryColor = Color(0xffE9D758);
+  static const darkThemeBackgroundColor = Color(0xff39393A);
+  static const darkThemeTextColor = Color(0xffE6E6E6);
+}
