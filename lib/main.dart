dfadd7f0686b868e4ba2be6d27cc746075fc1890
mahmoud-eslami <eslamimahmoud98@gmail.com>
2021-03-01 12:43:31 +0330

    application icon changed

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: Scaffold(),
     );
   }
 }
